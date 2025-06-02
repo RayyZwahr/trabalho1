bfae865b3d1c4e041df514745eb6edf329288a42
RayyZwahr <rayssazwahr1@gmail.com>
2025-06-02 10:42:02 -0300

    Update estruturas_condicao2.py

--- Microatividades/estruturas_condicao2.py
+++ Microatividades/estruturas_condicao2.py
@@ -1,8 +1,8 @@
-tempoExperiencia = 10
+tempoExperiencia = 10 #variavel que indica o tempo de experiência em numero.
 
-if tempoExperiencia <2 :
+if tempoExperiencia <2 : #se o tempo de experiencia for menor que dois anos
     print("nivel de conhecimento junior")
-elif tempoExperiencia >= 2  and tempoExperiencia <= 5:
+elif tempoExperiencia >= 2  and tempoExperiencia <= 5: #se o tempo de experiencia for entre dois a cinco anos
     print("nivel de conhecimento pleno") 
-else:
+else: #acima de 5 anos
     print("nivel de conhecimento senior")
