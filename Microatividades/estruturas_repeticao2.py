8c8c2542849a0ddd61105697e703f573fa8ee556
RayyZwahr <rayssazwahr1@gmail.com>
2025-06-02 10:46:28 -0300

    Update estruturas_repeticao2.py

--- Microatividades/estruturas_repeticao2.py
+++ Microatividades/estruturas_repeticao2.py
@@ -1,7 +1,7 @@
-texto = 'Olá, laço for'
+texto = 'Olá, laço for' #variável no formato string
 
-for item in texto:
-    print('Caractere: ' + item)
+for item in texto:  #laço for percorrendo a string
+    print('Caractere: ' + item)  #imprimime a letra
 
-for numero in range(1, 11):
-    print('Número do intervalo: ' + str(numero))
+for numero in range(1, 11): #novo laço que percorre os numeros de 1 a 11
+    print('Número do intervalo: ' + str(numero)) #imprime o numero correspondente a cada letra da string
