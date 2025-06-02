1c8fb6edd2e0a4a19a824105528171fc57a3692d
RayyZwahr <rayssazwahr1@gmail.com>
2025-06-02 10:44:32 -0300

    Update estruturas_repeticao.py

--- Microatividades/estruturas_repeticao.py
+++ Microatividades/estruturas_repeticao.py
@@ -1,7 +1,7 @@
-entrada_idade = ''
+entrada_idade = '' #variavel idade com valor em branco.
 
-while str(entrada_idade) != '0':
+while str(entrada_idade) != '0': #estrutura de repetição, que funciona enquanto a variavel entrada_idade for diferente de zero.
 
-    entrada_idade = input('Digite um número qualquer ou 0 para sair: ')
+    entrada_idade = input('Digite um número qualquer ou 0 para sair: ') #pede ao usuário um numero
     
-    print('Número digitado:', entrada_idade)
+    print('Número digitado:', entrada_idade) # impime o numero digitado
